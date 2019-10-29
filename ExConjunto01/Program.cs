610e41f19e3644f747b03033ecee54ec9ad4e7b2
William Henrique <williamhenriquecirino@hotmail.com>
2019-10-29 12:35:48 -0300

    Conjuntos - conceito de conjunto, declaração de um conjunto, Instanciação de um conjunto, adcionar um elemento no conjunto, percorrer um cojunto, testar se um objeto pertence ao conjunto, operações de conjunto: união, intersecção, diferença.

---
+++ ExConjunto01/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExConjunto01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conjunto
+            //intanciando um conjunto
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+
+            //Adciona elemnto dentro do conjunto
+            A.Add(3);
+            A.Add(5);
+            A.Add(8);
+            A.Add(9);
+
+            B.Add(3);
+            B.Add(4);
+            B.Add(5);
+
+            //remove elemento
+            // B.Remove(3);
+
+            //Retira a diferença de A em B
+            //A.ExceptWith(B);
+
+            //Uni o elementos de B ao A
+            //A.UnionWith(B);
+
+            //Mostra apenas os numeros repetidos dos conjuntos
+            //A.IntersectWith(B);
+
+            //Percore os conjuntos
+            //para cada int x dentro do conjunto A 
+            foreach (int x in A)
+            {
+                Console.WriteLine(x);
+            }
+
+            //Verificar se um numero digitado pertence 
+            Console.WriteLine("Digite um numero: ");
+            int N = int.Parse(Console.ReadLine());
+
+            if (B.Contains(N))
+            {
+                Console.WriteLine("Contem o numero " + N);
+            }
+            else
+            {
+                Console.WriteLine("Não contem o Numero " + N);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
