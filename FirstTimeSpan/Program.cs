881218c127b9ad9609d21dc442bae77cb8402942
William Henrique <williamhenriquecirino@hotmail.com>
2019-11-18 17:47:09 -0300

    First works with TimeSpan

---
+++ FirstTimeSpan/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = TimeSpan.MaxValue;    //value max from TimeSpan
+            TimeSpan t2 = TimeSpan.MinValue;    //Value min from TimeSpan
+            TimeSpan t3 = TimeSpan.Zero;        //Value zero from TimeSpan
+        }
+    }
+}
