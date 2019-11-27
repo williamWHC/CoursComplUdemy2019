7a11364fe5aaa1b0a4dca8bee66a60cc9be7ae91
William Henrique Cirino <williamhcirino@brq.com>
2019-11-27 16:13:17 -0300

    Created a new application in Composition

--- Composicao/Entities/Worker.cs
+++ Composicao/Entities/Worker.cs
@@ -10,8 +10,10 @@ namespace Composicao.Entities
         public string Name { get; set; }
         public WorkerLevel Level { get; set; }
         public double BaseSalary { get; set; }
+        //Class department associada a class department--V
         public Department Department { get; set; }
-        public HourContract Contract { get; set; }
+        //Associando com a Class HourContract
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();           //instanciar a lista para garantir que ela não seja nulla(null)
 
 
         //Construtor da Class
@@ -20,18 +22,38 @@ namespace Composicao.Entities
 
         }
 
-        public Worker(string name, WorkerLevel level, double basesalary)
+        public Worker(string name, WorkerLevel level, double basesalary, Department department) //Não incluir associações para muitos, por exemplo propriedade Contracts
         {
             Name = name;
             Level = level;
             BaseSalary = basesalary;
+            Department = department;
         }
 
         //metodos
         public void AddContract(HourContract contract)
         {
-             
+            this.Contracts.Add(contract);
         }
 
+        public void RemoveContract(HourContract contract)
+        {
+            this.Contracts.Remove(contract);
+        }
+
+        //Saber quanto em renda o trabalhador ganhou
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
     }
 }
--- Composicao/Program.cs
+++ Composicao/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using Composicao.Entities;
+using Composicao.Entities.Enums;
 
 namespace Composicao
 {
@@ -6,7 +9,29 @@ namespace Composicao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+
+            Console.Write("Enter worker data:");
+            Console.Write("Enter: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Level (Junior/Plano/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+
+            Console.Write("Base Salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, baseSalary, dept);
+
+            Console.WriteLine(worker);
+
+            Console.WriteLine("How many contracts to this worker? ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            
         }
     }
 }
