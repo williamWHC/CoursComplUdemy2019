7f399063bf9a0a1e43c06dcc586f5319796c95eb
William Henrique Cirino <williamhcirino@brq.com>
2020-03-05 16:22:07 -0300

    Worker with Try-Catch Exceptions

---
+++ ExempleException/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExempleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Digite um valor da multiplicação: ");
+                int n1 = int.Parse(Console.ReadLine());
+                Console.Write("Digite um valor 2 da multiplicação: ");
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erro foi difgitado outra tipo de dado");
+                Console.ReadKey();
+                throw;
+            }
+          
+
+            
+
+
+        }
+    }
+}
