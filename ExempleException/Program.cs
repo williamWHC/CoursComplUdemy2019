74e4dc00403ba569d24fdc64609df3a8697f184f
William Henrique Cirino <williamhcirino@brq.com>
2020-03-09 15:49:47 -0300

    New update script try-carch

--- ExempleException/Program.cs
+++ ExempleException/Program.cs
@@ -6,6 +6,9 @@ namespace ExempleException
     {
         static void Main(string[] args)
         {
+
+            //Com o tratamento de expection passando como paramento no catch a exception DivideByZeroException
+            //O proprio usuario pode definir a mensagem de erro, substituindo no ConseleAplication.
             try
             {
                 Console.Write("Digite um valor da multiplicação: ");
@@ -16,12 +19,33 @@ namespace ExempleException
                 int result = n1 / n2;
                 Console.WriteLine(result);
             }
-            catch (FormatException)
+            catch (DivideByZeroException)
             {
-                Console.WriteLine("Erro foi difgitado outra tipo de dado");
-                Console.ReadKey();
+                Console.WriteLine("Erro de multiplicação !!");
                 throw;
             }
+
+
+            //O tratamento de Exception por formatexception você pode apenas definir a mensagem
+            //Com o função (Message) para apenas mostrar para o usuario a mensagem de erro. 
+            /*
+            try
+            {
+                Console.Write("Digite um valor da multiplicação: ");
+                int n1 = int.Parse(Console.ReadLine());
+                Console.Write("Digite um valor 2 da multiplicação: ");
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Mensagem de Erro -> " + e.Message);
+                throw;
+            }
+            */
+            
           
 
             
