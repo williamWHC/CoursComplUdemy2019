fcb0e0b35973ee9d917f7f02ccad03f38aec8d17
William Henrique Cirino <williamhcirino@brq.com>
2020-04-27 16:15:27 -0300

    Examples with StreamWrite

---
+++ StreamWrite/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace StreamWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\willi\source\repos\williamWHC\CoursComplUdemy2019\file1.txt";
+            string targetPath = @"C:\Users\willi\source\repos\williamWHC\CoursComplUdemy2019\file2.txt";
+
+            try
+            {
+                //vetor de todos os vetores
+                string[] lines = File.ReadAllLines(sourcePath);
+
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        Console.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occuured"); //Um erro occoreu
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
