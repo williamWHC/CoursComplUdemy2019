9e26b878af396e5647efbc874537aab8ef53872a
William Henrique Cirino <williamhcirino@brq.com>
2020-04-27 10:11:04 -0300

    Class StreamReadear, copying all the content of the file TXT and then showing in the screen.

---
+++ FileStreamAndStreamReader/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace FileStreamAndStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Worker with FileStream and StreamReader
+            string path = @"C:\Windows\Temp\file1.txt";
+            StreamReader sr = null;
+
+            try
+            {
+                //sr = new StreamReader(fs);
+                //string line = sr.ReadLine();
+                //Console.WriteLine(line);
+                //defini o fluxo do arquivo e chama o metodo de modo de arquivo aberto
+                //le o arquivo, captura a primeira linha do arquivo capturado
+                //fs = new FileStream(path, FileMode.Open);
+
+                sr = File.OpenText(path);           //Abre um arquivo de texto codificado
+                while (!sr.EndOfStream)             //enquanto o sr não chegar no fim do fluxo, mostre 
+                {
+                    string lines = sr.ReadLine();
+                    Console.WriteLine(lines);
+                    Console.ReadKey();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An erro accurred"); //Um erro ocorreu
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            finally
+            {
+                //if (sr != null) sr.Close();
+                //if (fs != null) sr.Close();
+            }
+            
+        }
+    }
+}
